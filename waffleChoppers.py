3c34d93eca717f1dc6ca67aa76b9161e3df1f655
duncanlindsey <duncan.m.lindsey@gmail.com>
2018-04-29 00:24:10 +0100

    Building the waffle solution

--- waffleChoppers.py
+++ waffleChoppers.py
@@ -1,5 +1,41 @@
 import helperFunctions as hf
 
+''' Functions '''
+
+def solve():
+    pass
+
+''' Main '''
+
 # Import test data
 data = hf.import_data('exampleData')
-hf.print_list(data)
+
+# Track data input index
+index = 0
+
+# Read first line
+#T = int(input()) #SWAP
+T = int(data[index]) #SWAP
+index += 1 #SWAP
+
+# Loop through test cases
+for t in range(1, T+1):
+
+    # Read variable line
+    #data_line = input() #SWAP
+    data_line = data[index] #SWAP
+    index += 1 #SWAP
+
+    # Chop variable line into list
+    variables = data_line.split(' ')
+
+    # Read content lines
+    matrix = []
+    for _ in range(int(variables[0])):
+        #content_line = input() #SWAP
+        content_line = data[index] #SWAP
+        index += 1 #SWAP
+        matrix.append(content_line.split(' '))
+    
+    print ('Test #%s\nVariables:%s\nMatrix:' % (t, variables))
+    hf.print_list(matrix)
